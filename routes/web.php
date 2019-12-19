676f13d571eb251d0b19dafdd2c15c29d72b3a2d
zly <zly@localhost.localdomain>
2019-12-19 20:32:18 +0800

    首页

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,14 @@
 |
 */
 
+/*
 Route::get('/', function () {
-
     return view('welcome');
 });
+*/
+
+Route::get('/','Index\IndexController@index');	//网站首页
+
 
 Route::get('/info',function(){
 	phpinfo();
