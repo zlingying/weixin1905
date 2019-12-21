039ab351192e39f2c9fa301159010ac04b934a34
zly <zly@localhost.localdomain>
2019-12-21 09:48:03 +0800

    商品管理

--- app/Http/Controllers/Goods/IndexController.php
+++ app/Http/Controllers/Goods/IndexController.php
@@ -5,13 +5,21 @@ namespace App\Http\Controllers\Goods;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Model\GoodsModel;
+
 class IndexController extends Controller
 {
 	/*
 		商品详情页
 	 */
-    public function detail()
+    public function detail(Request $request)
     {
-    	return view('goods.detail');
+    	$goods_id = $request->input('id');
+    	$goods = GoodsModel::find($goods_id);
+    	//echo '<pre>';print_r($goods);echo '</pre>';
+    	$data = [
+    		'goods' => $goods
+    	];
+    	return view('goods.detail',$data);
     }
 }
