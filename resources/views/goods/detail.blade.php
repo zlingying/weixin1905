c0799711128fcc8a64e8523ef865fa98f67f0b28
zly <zly@localhost.localdomain>
2019-12-21 11:17:49 +0800

    富文本编辑器

--- resources/views/goods/detail.blade.php
+++ resources/views/goods/detail.blade.php
@@ -369,6 +369,7 @@
 				<img src="/storage/{{$goods->img}}" alt="">
 				<h5>Fashion Men's</h5>
 				<div class="price">$20 <span>$28</span></div>
+				{!! $goods->desc !!}
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam eaque in non delectus, error iste veniam commodi mollitia, officia possimus, repellendus maiores doloribus provident. Itaque, ab perferendis nemo tempore! Accusamus</p>
 				<button type="button" class="btn button-default">ADD TO CART</button>
 			</div>
