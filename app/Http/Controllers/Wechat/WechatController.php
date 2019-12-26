9d4aaf5fd9a0719ac47c9661b958a0d33eb7b2b1
zly <zly@localhost.localdomain>
2019-12-26 10:53:57 +0800

    考试

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -143,7 +143,7 @@ class WechatController extends Controller
           //openid入库
           $uid = WxUserModel::insertGetId($user_data);
 
-          $msg = "欢迎. $u['nickname'] .同学进入选课系统";
+          $msg = "欢迎".$u['nickname']."同学进入选课系统";
           //回复用户关注
              $xml = '<xml>
                         <ToUserName><![CDATA['.$openid.']]></ToUserName>
@@ -325,10 +325,10 @@ class WechatController extends Controller
     public function CreateMenu()
     {
 
-        $url = 'http://zly.xx20.top/vote';
-        $url2 = 'http://zly.xx20.top/wx/login';
-        $redirect_uri = urlencode($url);    //授权后跳转页面
-        $redirect_uri2 = urlencode($url2);
+        // $url = 'http://zly.xx20.top/vote';
+        // $url2 = 'http://zly.xx20.top/wx/login';
+        // $redirect_uri = urlencode($url);    //授权后跳转页面
+        // $redirect_uri2 = urlencode($url2);
 
       //创建自定义菜单的接口地址
       $url = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token='.$this->access_token;
